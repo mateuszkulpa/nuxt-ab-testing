fd3134cc4c1a6a4159f228daa1cf4a431fb1ca3b
Mateusz Kulpa <mateusz.kulpa@outlook.com>
2024-04-03 16:42:58 +0200

    feat: add support for persisting variants resolved on the server

--- src/runtime/server/routes/resolve-variant.get.ts
+++ src/runtime/server/routes/resolve-variant.get.ts
@@ -1,11 +1,22 @@
-import { defineEventHandler, getValidatedQuery, createError, useRuntimeConfig } from '#imports'
+import {
+  defineEventHandler,
+  getValidatedQuery,
+  createError,
+  useRuntimeConfig,
+  useStorage,
+  parseCookies,
+  setCookie,
+} from '#imports'
 import { resolveABTestVariant } from '../../core/variants'
-
 import { z } from 'zod'
+import { v4 as uuidv4 } from 'uuid'
 
 const querySchema = z.object({
   id: z.string(),
 })
+
+const STORAGE_BASE_KEY = 'ab-testing'
+const COOKIE_PERSISTENCE_KEY = `nuxt-ab-testing`
 
 export default defineEventHandler(async event => {
   const runtimeConfig = useRuntimeConfig()
@@ -28,5 +39,24 @@ export default defineEventHandler(async event => {
     })
   }
 
-  return resolveABTestVariant(abTest)
+  if (!runtimeConfig.public.abTesting.persistVariants) {
+    return resolveABTestVariant(abTest)
+  }
+
+  const storage = useStorage(STORAGE_BASE_KEY)
+  let persistenceKey = parseCookies(event)[COOKIE_PERSISTENCE_KEY]
+  if (!persistenceKey) {
+    persistenceKey = uuidv4()
+    setCookie(event, COOKIE_PERSISTENCE_KEY, persistenceKey)
+  }
+
+  const persistedVariantKey = `${persistenceKey}-${abTest.id}`
+
+  if (!(await storage.hasItem(persistedVariantKey))) {
+    const selectedVariant = resolveABTestVariant(abTest)
+    await storage.setItem(persistedVariantKey, selectedVariant)
+    return selectedVariant
+  }
+
+  return await storage.getItem(persistedVariantKey)
 })
